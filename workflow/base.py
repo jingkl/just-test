396a8749c45a0f1e9b54f7636eb5d29bdbf4949a
wangting0128 <ting.wang@zilliz.com>
2023-06-21 13:47:43 +0800

    update code

--- workflow/base.py
+++ workflow/base.py
@@ -78,8 +78,8 @@ class Base:
     def pop_specified_func(self, father_funcs: list, pop_func: callable):
         _subscript = None
         for f in range(len(father_funcs)):
-            print(father_funcs[f], father_funcs[f][0], pop_func)
-            if father_funcs[f][0] == pop_func:
+            print(father_funcs[f], father_funcs[f][0][0], pop_func, father_funcs[f][0][0] == pop_func)
+            if father_funcs[f][0][0] == pop_func:
                 _subscript = f
                 break
 
