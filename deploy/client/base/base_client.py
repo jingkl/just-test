2b23eaed24586c433095da812b563865d616cd1c
jingkl <jingjing.jia@zilliz.com>
2023-07-06 10:52:09 +0800

    add resume
    Signed-off-by: jingkl <jingjing.jia@zilliz.com>

--- deploy/client/base/base_client.py
+++ deploy/client/base/base_client.py
@@ -24,9 +24,9 @@ class BaseClient(metaclass=abc.ABCMeta):
     def delete_pvc(self, *args, **kwargs):
         log.debug("[BaseClient] Delete pvc: {}".format(args, kwargs))
     
-    @abc.abstractmethod
-    def resume_server(self, *args, **kwargs):
-        log.debug("[BaseClient] resume pods: {}".format(args, kwargs))
+    # @abc.abstractmethod
+    # def resume_server(self, *args, **kwargs):
+    #     log.debug("[BaseClient] resume pods: {}".format(args, kwargs))
 
     @abc.abstractmethod
     def endpoint(self, *args, **kwargs):
