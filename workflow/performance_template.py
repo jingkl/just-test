e7fa4ffba0355d64b54e54f00aa806df555dceea
jingkl <jingjing.jia@zilliz.com>
2023-07-17 22:47:38 +0800

    modify resume
    Signed-off-by: jingkl <jingjing.jia@zilliz.com>

--- workflow/performance_template.py
+++ workflow/performance_template.py
@@ -177,11 +177,11 @@ class ServerTemplate(Base):
         else:
             self.init_server_client(deploy_tool=input_params.deploy_tool, deploy_mode=input_params.deploy_mode)
         
-        if param_info.deploy_resume and param_info.deploy_skip:
-            self.resume_server(deploy_resume=input_params.deploy_resume)
+        # if param_info.deploy_resume and param_info.deploy_skip:
+        #     self.resume_server(deploy_resume=input_params.deploy_resume)
 
-        # if not param_info.deploy_retain:
-        #     self.deploy_delete(deploy_retain_pvc=param_info.deploy_retain_pvc, deploy_uninstall=deploy_uninstall)
+        if not param_info.deploy_retain:
+            self.deploy_delete(deploy_retain_pvc=param_info.deploy_retain_pvc, deploy_uninstall=deploy_uninstall)
 
     def upgrade_server_template(self, input_params: InputParamsBase, release_name=None, deploy_mode=STANDALONE,
                                 upgrade_config: str = ""):
